0bc5eab0efb5b5b04b177d6d522720b21447e128
17819 <17819@LAPTOP-52VSKCAI>
2023-05-05 21:19:07 -0400

    hjjjjn

---
+++ src/Marketing/Welocome.java
@@ -0,0 +1,11 @@
+package Marketing;
+
+public class Welocome {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone");
+		System.out.println("welcome to code lab tech");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 17819
+ *
+ */
+module SecondProject {
+}
